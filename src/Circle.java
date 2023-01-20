0bf7c72289cec370fd914a93004a09dfe8391ead
tokto <toktonalievaalbina72@gmail.com>
2023-01-20 17:24:46 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,15 @@
+public class Circle {
+    private int PI;
+    private int radius = 3;
+
+    public static double area (){
+       double area = Math.PI * (3*3);
+       return area;
+    }
+    public static double  circumference (){
+        double circumference = Math.PI* 2 * 3;
+        return circumference;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    Circle.area();
+    Circle.circumference();
+        System.out.println("площадь круга равна:" + Circle.area());
+        System.out.println("днилы окружности равна:" + Circle.circumference());
+
+
+    }
+}
